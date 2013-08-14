0ac9c99731d4a7c90956a6b0db4763ba949ebcb7
Rory Primrose <rory@neovolve.com>
2013-08-14 10:51:38 +1000

    Added untracked file

---
+++ Headless.IntegrationTests/Redirect.cs
@@ -0,0 +1,25 @@
+﻿namespace Headless.IntegrationTests
+{
+    using System;
+
+    /// <summary>
+    ///     The <see cref="Redirect" />
+    ///     class provides the locations of addresses under the redirect controller.
+    /// </summary>
+    public static class Redirect
+    {
+        /// <summary>
+        ///     Gets the index address.
+        /// </summary>
+        /// <value>
+        ///     The index address.
+        /// </value>
+        public static Uri Index
+        {
+            get
+            {
+                return new Uri(Config.BaseWebAddress, "redirect/index");
+            }
+        }
+    }
+}
